3baafb2714575318881458c3d4c4a53b9d2f87d9
ARIF MULANI <amnetwork049@gmail.com>
2024-02-19 08:52:11 +0530

    first commit

---
+++ Section-3_Graph_Representation/1-Find_center_of_star_graph/1-find-center-of-star-graph.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findCenter(vector<vector<int>>edges)
+{
+    if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1]) return edges[0][0];
+    else return edges[0][1];
+}
